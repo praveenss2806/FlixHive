02e79885d39fc0de2ab97876fdf5a1b9293f97f3
Praveen S <praveenss2806@gmail.com>
2023-11-08 20:33:06 +0530

    fetched popular movies data from tmdb api

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@ const globalObj = {
     currentPage: window.location.pathname,
 };
 
+const getPopularMovies = async () => {
+    const { results } = await fetchAPIdata('movie/popular');
+    console.log(results);
+};
+
+//fetch the data from TMDB api
+const fetchAPIdata = async (endpoint) => {
+    apiUrl = 'https://api.themoviedb.org/3';
+    apiKey = 'f9722bd6bf6f645f6ce0506a98ce6067';
+
+    const response = await fetch(
+        `${apiUrl}/${endpoint}?api_key=${apiKey}&language=en-US`
+    );
+
+    const data = await response.json();
+
+    return data;
+};
+
 //highlight active link
 const hlActiveLink = () => {
     const navLinks = document.querySelectorAll('.nav-link');
@@ -17,6 +36,7 @@ const init = () => {
     switch (globalObj.currentPage) {
         case '/':
         case '/index.html':
+            getPopularMovies();
             console.log('FlixHive Home');
             break;
         case '/shows.html':
