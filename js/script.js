2023db42321c76d035e1f4aee178b81fadac16d7
Praveen S <praveenss2806@gmail.com>
2023-12-04 12:13:03 +0530

    added slider

--- js/script.js
+++ js/script.js
@@ -216,6 +216,52 @@ const getShowDetails = async () => {
     document.querySelector('#show-details').appendChild(div);
 };
 
+const showSlider = async () => {
+    const { results } = await fetchApiData('movie/now_playing');
+
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+            <img src="https://image.tmdb.org/t/p/w500${
+                movie.poster_path
+            }" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+            <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+                1
+            )} / 10
+            </h4>
+        `;
+
+        document.querySelector('.swiper-wrapper').appendChild(div);
+
+        initSwiper();
+    });
+};
+
+const initSwiper = () => {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        breakpoints: {
+            500: {
+                slidesPerView: 2,
+            },
+            700: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 4,
+            },
+        },
+    });
+};
+
 const showSpinner = () => {
     document.querySelector('.spinner').classList.add('show');
 };
@@ -256,6 +302,7 @@ const init = () => {
     switch (globalObj.currentPage) {
         case '/':
         case '/index.html':
+            showSlider();
             getPopularMovies();
             break;
         case '/shows.html':
